b11cad59e3fcf396e5fbffb91ad7d5872641320e
zakenmaru <sohammukherjee2000@gmail.com>
2020-11-01 01:28:05 -0500

    finished instructions button and template for Options

--- frontend/go-phish/src/App.js
+++ frontend/go-phish/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import title_logo from './title_logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+
+  const [state, setState] = useState(1);
+
+  const [hintstate, setHintState] = useState(1);
+
+  const states = () => {
+    /* Home Page */
+    if (state === 1)
+    {
+      return (
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <title>
+          Go Phish
+        </title>
+        <img src={title_logo} />
+        <div className="padding"></div>
+        <div className="button">
+          Play
+        </div>
+        <div className="padding"></div>
+          <div className="button" onClick={() => setState(2)}>
+          Instructions
+        </div>
+        <div className="padding"></div>
+        <div className="button" onClick={() => setState(3)}>
+          Options
+        </div>
       </header>
-    </div>
-  );
+      );
+    }
+    /* Instructions Page */
+    if (state === 2) {
+      return (
+      <div>
+        <header class="App-header">
+          <img src={title_logo} alt="title" class="responsive" />
+          <h1>Instructions</h1>
+          <p>
+            Welcome to Go Phish, a point-and-click game
+            where you identify phishing attempts within the time limit!
+            Drag and drop images one by one into two buckets: Phishing and Not,
+            but try not to get baited in this quick, addictive game!
+        </p>
+          <div className="button" onClick={() => setState(1)}>
+            Go back
+        </div>
+        </header>
+      </div>
+      );
+    }
+    /* Options */
+    if (state === 3) {
+      return (
+        <div>
+          <header class="App-header">
+            <h1>Options</h1>
+            <div className="button" onClick={() => setState(1)}>
+              Go back
+            </div>
+
+          </header>
+        </div>
+      );
+    }
+    /* Game State */
+    if (state === 4) {
+
+    }
+  }
+  return (states());
 }
 
 export default App;
